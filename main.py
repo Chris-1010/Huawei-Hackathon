9b8a3ce28bf261b84ddcef8c260bfdbd41ccd02a
Christopher Ahern <115749466+Chris-1010@users.noreply.github.com>
2023-09-23 13:24:37 +0100

    query_fun function filled & started tokenising

--- main.py
+++ main.py
@@ -2,13 +2,16 @@
 To recap, you are here to build an AI Data Assistant. Your co-pilot for the semi-structured data that many companies has.
  The copilot should make life of Human Data Assistant easier by getting insights from the data it has access to.
 """
-
+# pip install transformers, nltk, pytorch
 import sqlite3
+from transformers import BertTokenizer
+from nltk.corpus import stopwords
+
 class DatabaseAI(object):
     def __init__(self, database_name:str):
         self._database_name = database_name
 
-    def connect_fun(self):
+    def connect_fun(self, database_name:str):
         """Object that should “establish a connection” to a given SQLite file. 
             That function will be run once before all the questions related to the given database"""
         #Connect to SQLite
@@ -16,7 +19,7 @@ class DatabaseAI(object):
         cursor = conn.cursor()
 
         #Execute a query
-        rows = cursor.execute("""SELECT * FROM covid_vaccinations LIMIT 1;""").fetchall()
+        rows = cursor.execute(f"SELECT * FROM {database_name};").fetchall()
 
 
         #Close connection
@@ -27,13 +30,23 @@ class DatabaseAI(object):
 
     def query_fun(question:str, tables:list[str], database_connection: object):
         """This function will be run once for each question related to the given database."""
-        return
+
+        #Tokenize the question - in order for it to be read by the ai
+        tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")  #create the tokenizer
+        stop_words = set(stopwords.words("english"))         #set of english stop words like commas, full stops and question marks
+        tokens = tokenizer.tokenize(question)
+        for token in tokens:
+            if token in stop_words:
+                tokens.remove(token)            #removing any stop words so that ai doesn't need to process them and can read more efficiently  
 
 
-databases = {"covid_vaccinations": "example-data/example-covid-vaccinations.sqlite3", "simple_users": "example-data/example-simple.sqlite3", "murder_mystery": "example-data/sql-murder-mystery.sqlite3"}
-database = databases[input("Would you like to use the database of:\n'covid_vaccinations',\t'',\t'simple_users'")] # sub in whatever database you'd like instead of "simple_users"
+databases = {"covid_vaccinations": "example-data/example-covid-vaccinations.sqlite3", "orders": "example-data/example-simple.sqlite3", "murder_mystery": "example-data/sql-murder-mystery.sqlite3"}
+preferred_database = input("Would you like to use the database of:\n'covid_vaccinations',\t'orders', or\t'murder_mystery'\n\n")
+database = databases[preferred_database]
 the_ai = DatabaseAI(database)
-print(the_ai.connect_fun())
+rows = (the_ai.connect_fun(preferred_database))
+print(rows)
 
-# from datasets import load_dataset
-# dataset = load_dataset("fka/awesome-chatgpt-prompts")
+#tokenizing
+question = input("Enter your question: ")
+the_ai.query_fun(question, rows)
