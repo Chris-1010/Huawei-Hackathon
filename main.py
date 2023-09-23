4b7ce5b84e3f1e7520457009c6a291579ab6598b
john-david3 <127981157+john-david3@users.noreply.github.com>
2023-09-23 13:49:03 +0100

    Add files via upload
    Cleaned up

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ To recap, you are here to build an AI Data Assistant. Your co-pilot for the semi
 # pip install transformers, nltk, pytorch
 import sqlite3
 from transformers import BertTokenizer
+import nltk
 from nltk.corpus import stopwords
+
+# nltk.download("stopwords")
 
 class DatabaseAI(object):
     def __init__(self, database_name:str):
@@ -28,7 +31,7 @@ class DatabaseAI(object):
 
         return rows
 
-    def query_fun(question:str, tables:list[str], database_connection: object):
+    def query_fun(self, question:str, tables:list[str]):
         """This function will be run once for each question related to the given database."""
 
         #Tokenize the question - in order for it to be read by the ai
@@ -42,9 +45,9 @@ class DatabaseAI(object):
 
 databases = {"covid_vaccinations": "example-data/example-covid-vaccinations.sqlite3", "orders": "example-data/example-simple.sqlite3", "murder_mystery": "example-data/sql-murder-mystery.sqlite3"}
 preferred_database = input("Would you like to use the database of:\n'covid_vaccinations',\t'orders', or\t'murder_mystery'\n\n")
-database = databases[preferred_database]
-the_ai = DatabaseAI(database)
-rows = (the_ai.connect_fun(preferred_database))
+db_path = databases[preferred_database]
+the_ai = DatabaseAI(db_path)
+rows = the_ai.connect_fun(preferred_database)
 print(rows)
 
 #tokenizing
