731d26dc51ae2f0c8b89859f954d9d0c2eb7f157
M-dok <145792723+M-dok@users.noreply.github.com>
2023-09-23 14:56:19 +0100

    Update JSON_reader.py

--- JSON_reader.py
+++ JSON_reader.py
@@ -5,28 +5,31 @@ import json
 class DataJS:
     def __init__(self,file_name):
         self.file_name = file_name
-        self.data = []
+        self.data = []                                      #Stores data of individual files 
+        #self.get_data()
         
-    def get_data(self): #Places content into data
+    def get_data(self):
+        self.file_name = os.path.join("example-data/",self.file_name)                                     #Places content into data
         if path.isfile(self.file_name) is False:
             raise Exception("File not found")
-        f = open(self.file_name,"r") #opens file 
-        der = json.load(f)   # places contents of the file in der 
-        self.data = der       #places content into instance var 
+        f = open(self.file_name,"r")                        #opens file 
+        der = json.load(f)                                  # places contents of the file in der 
+        self.data = der                                     #places content into instance var 
         f.close()
         return self.data
 class Files:
     def __init__(self):
-        self.files = []
+        self.files = []                                     #Stores all the instances of DataJS
     def add_to_list(self):
-            for i in os.listdir("example-data"):
+            for i in os.listdir("example-data"):            #goes through list of files in "example-data" folder
                 if ".json" in i:
-                    #i = DataJS(i)
-                    self.files.append(i)
+                    der = DataJS(i)
+                    self.files.append(der)
         
 if __name__ == "__main__":
     f = Files()
-    #print(os.listdir("example-data"))
+    f.add_to_list()
+    print(f.files[0].get_data())
 
 
 
