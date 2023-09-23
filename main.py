b94f640506dca4f5a67433a77aa75c7013198fcf
john-david3 <127981157+john-david3@users.noreply.github.com>
2023-09-23 13:02:38 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 To recap, you are here to build an AI Data Assistant. Your co-pilot for the semi-structured data that many companies has.
  The copilot should make life of Human Data Assistant easier by getting insights from the data it has access to.
 """
-
 import sqlite3
+from transformers import BertTokenizer
+from nltk.corpus import stopwords
+
 class DatabaseAI(object):
     def __init__(self, database_name:str):
         self._database_name = database_name
@@ -19,12 +21,28 @@ class DatabaseAI(object):
         cursor.execute("""SELECT * FROM covid_vaccinations""")
         rows = cursor.fetchall()
 
-
         #Close connection
         cursor.close()
         conn.close()
 
-    def query_fun(question:str, tables:list[str], database_connection: object):
+        return rows
+
+    def query_fun(self, question:str, tables:list[str]):
         """This function will be run once for each question related to the given database."""
+        #Tokenize the question - in order for it to be read by the ai
+        tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")  #create the tokenizer
+        stop_words = set(stopwords.words("english"))         #set of english stop words like commas, full stops and question marks
+        tokens = tokenizer.tokenize(question)
+        for token in tokens:
+            if token in stopwords:
+                tokens.remove(token)            #removing any stop words so that ai doesn't need to process them and can read more efficiently  
+
+    
+        
+
 
 user = DatabaseAI("app.db")
+rows = user.connect_fun()
+
+question = input("Enter your question: ")
+user.query_fun(question, rows)
