61f5616daf8ec3dafdf88f49d3d6eca853bcbb00
M-dok <145792723+M-dok@users.noreply.github.com>
2023-09-23 14:01:25 +0100

    Update JSON_reader.py

--- JSON_reader.py
+++ JSON_reader.py
@@ -1,18 +1,32 @@
 from os import path
+import os 
 import json 
-class dataJS:
+        
+class DataJS:
     def __init__(self,file_name):
         self.file_name = file_name
         self.data = []
-    def help(self):
+        
+    def get_data(self): #Places content into data
         if path.isfile(self.file_name) is False:
             raise Exception("File not found")
         f = open(self.file_name,"r") #opens file 
         der = json.load(f)   # places contents of the file in der 
         self.data = der       #places content into instance var 
-        
         f.close()
+        return self.data
+class Files:
+    def __init__(self):
+        self.files = []
+    def add_to_list(self):
+            for i in os.listdir("example-data"):
+                if ".json" in i:
+                    #i = DataJS(i)
+                    self.files.append(i)
+        
+if __name__ == "__main__":
+    f = Files()
+    #print(os.listdir("example-data"))
 
-test1 = dataJS("example-data\example-covid-vaccinations.json")
-test1.help()
-print(test1.data)
+
+
